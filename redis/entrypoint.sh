827114bf40311a27fe8cc1e0fc4a37c8fb40ba00
Jonathon W. Marshall <jmarshall@bigvikinggames.com>
2016-04-20 15:03:42 -0400

    use bash; overwrite existing entrypoint.sh to avoid confusion

--- redis/entrypoint.sh
+++ redis/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 # This is a modified version of the official redis docker-entrypoint.sh
@@ -12,15 +12,14 @@ REDIS_REQUIREPASS=${REDIS_REQUIREPASS:-redis}
 
 export REDIS_MAXCLIENTS REDIS_MAXMEMORY REDIS_MAXMEMORY_POLICY REDIS_REQUIREPASS
 
-eval "cat <<EOF
-$(</redis.conf.tmpl)
-EOF
-" > /data/redis.conf
+eval "cat <<-EOF
+	$(</redis.conf.tmpl)
+	EOF" > /data/redis.conf
 
 # allow the container to be started with `--user`
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
-    chown -R redis .
-    exec gosu redis "$0" "$@"
+	chown -R redis .
+	exec gosu redis "$0" "$@"
 fi
 
 exec "$@"
